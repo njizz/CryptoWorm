4e4d3eab161f8f73fa2323dcde70d6f86205d184
njizz <njizzard@gmail.com>
2022-11-22 17:08:43 +0000

    Added known-addresses and block pagination

--- index.js
+++ index.js
@@ -27,6 +27,24 @@ app.get('/api/blocks', (req, res) => {
     res.json(blockchain.chain);
 });
 
+app.get('/api/blocks/length', (req, res) => {
+    res.json(blockchain.chain.length);
+});
+
+app.get('/api/blocks/:id', (req, res) => {
+    const { id } = req.params;
+    const { length } = blockchain.chain;
+    const blocksReversed = blockchain.chain.slice().reverse();
+
+    let startIndex = (id-1) * 5;
+    let endIndex = id * 5;
+
+    startIndex = startIndex < length ? startIndex : length;
+    endIndex = endIndex < length ? endIndex : length;
+
+    res.json(blocksReversed.slice(startIndex, endIndex));
+});
+
 app.post('/api/mine', (req, res) => {
     const { data } = req.body;
 
@@ -80,6 +98,20 @@ app.get('/api/wallet-info', (req, res) => {
         address,
         balance: Wallet.calculateBalance({ chain: blockchain.chain, address })
     });
+});
+
+app.get('/api/known-addresses', (req, res) => {
+    const addressMap = {};
+    
+    for (let block of blockchain.chain) {
+        for (let transaction of block.data) {
+            const recipient = Object.keys(transaction.outputMap);
+
+            recipient.forEach(recipient => addressMap[recipient] = recipient);
+        };
+    };
+
+    res.json(Object.keys(addressMap));
 });
 
 app.get('*', (req, res) => {
